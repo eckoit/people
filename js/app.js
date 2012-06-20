85ebc63269b29dc77a50b63926710c04f0731a0b
default_git_name <default_git_email>
2012-06-20 19:00:19 +0000

    Foundation

--- js/app.js
+++ js/app.js
@@ -2,25 +2,35 @@ define('js/app',[
     'jquery',
     'handlebars',
     'couchr',
-    'underscore'
+    'underscore',
+	'director'
 
 ],
-function($, handlebars, couch,  _){
+function($, handlebars, couch,  _, director){
     var exports = {};
     var templates = {};
+    var router;
 
     function compileTemplate(domId, templateName) {
         var source   = $("#" + domId).html();
         templates[templateName] = handlebars.compile(source);
     }
 
+	function scan() {
+		$('.content').html(templates['my.html']({}));
+	}
+
+
     exports.init = function () {
-        compileTemplate('my-template', 'my.html');
+		compileTemplate('my-template', 'my.html');
+        router = director.Router({
+			'/scan' : scan
+		});
+		router.init('/scan');
     };
 
-
     exports.ui = function () {
-		$('.message').text('Welcome');
+		
     };
 
     return exports;
