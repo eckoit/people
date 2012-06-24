5e1d4b2ba4570c5a9ac8b7e2e9fc03d69d85be81
default_git_name <default_git_email>
2012-06-24 20:34:20 +0000

    o d s

--- js/app.js
+++ js/app.js
@@ -1,61 +1,42 @@
 define('js/app',[
     'jquery',
-    'handlebars',
-    'couchr',
     'underscore',
-	'director',
-    'text!templates/fast_add.html',
-    'text!templates/README.md',
-    'pouchdb'
-
+    'pouchdb',
+    'handlebars',    
+	  'director',
+	  'EpicEditor',
+    'text!templates/intro.html'
 ],
-function($, handlebars, couch,  _, director, fast_add_html,  readme, Pouch){
+function( $,  _, Pouch, handlebars, director, EpicEditor, intro_html){
     var exports = {};
     var templates = {};
     var router;
+    
+    templates['intro.html'] = handlebars.compile(intro_html);
 
 
+	  function scan() {
+	  }
 
-    var authors = [
-     {name: 'Dale Harvey', commits: 253},
-     {name: 'Mikeal Rogers', commits: 42},
-     {name: 'Johannes J. Schmidt', commits: 13},
-     {name: 'Randall Leeds', commits: 9}
-   ];
-   Pouch('idb://authors', function(err, db) {
-     // Opened a new database
-     db.allDocs({include_docs: true}, function(err, results) {
-       // Saved the documents into the database
-       console.log('docs: ', results)
-     });
-   });
-
-    templates['fast_add.html'] = handlebars.compile(fast_add_html);
-
-    function compileTemplate(domId, templateName) {
-        var source   = $("#" + domId).html();
-        templates[templateName] = handlebars.compile(source);
-    }
-
-	function scan() {
-		$('.content').html(templates['my.html']({}));
-	}
-
-    function fast_add() {
-        $('.content').html(templates['fast_add.html']({}));
+    function intro() {
+        $('.content').html(templates['intro.html']({}));
         //var editor = new EpicEditor().load();
-
-
     }
 
+	
 
     exports.init = function () {
-		compileTemplate('my-template', 'my.html');
         router = director.Router({
-			'/scan' : scan,
-            '/add' : fast_add
-		});
-		router.init('/add');
+          '/intro' : intro,
+          '/edit/:id' : intro,
+				'/edit/:id/publish' : intro,
+          '/edit/:id/trailhead' : intro,
+          '/edit/:id/chapter/:chapter' : intro,
+          '/play/:id' : intro,
+          '/play/:id/chapter/:chapter' : intro,
+				'/map' : intro,
+			});
+		router.init('/intro');
     };
 
     exports.ui = function () {
@@ -64,6 +45,7 @@ function($, handlebars, couch,  _, director, fast_add_html,  readme, Pouch){
 
     return exports;
 });
+
 
 
 
